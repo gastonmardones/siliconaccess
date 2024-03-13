72430a76be6c76d0a84a55e153f725b9ff2f6ec2
Gaston Mardones <gastonmardones22@gmail.com>
2024-03-13 17:33:51 -0300

    Initialize superuser in render.com

--- silicon_access/build.sh
+++ silicon_access/build.sh
@@ -7,3 +7,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Si el superusuario no existe, crearlo
+echo "Inicializando superusuario"
+echo "from django.contrib.auth.models import User; User.objects.filter(username='silicon').exists() or User.objects.create_superuser('silicon', 'silicon@access.com','access' )" | python manage.py shell
+echo "Supuerusuario creado"
